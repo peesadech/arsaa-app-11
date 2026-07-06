47bda1f1f584cb09afad8a1d2d005a0b55537ef8
Peesadech Paopongklay <peesadech11@gmail.com>
2026-07-06 16:50:59 +0700

    update acedamic result year / semester

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,6 +33,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             LanguageSeeder::class,
             StudentMasterDataSeeder::class,
+            AttendanceStatusSeeder::class,
         ]);
     }
 }
